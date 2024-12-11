a30c67e65c92af4ab4912459ed38fd90dc0f3183
carloslopez04 <clmlopezcarlos@gmail.com>
2024-12-11 13:04:14 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st 
 import pandas as pd
-import joblib 
+import joblib
+import json 
 
 
 st.title("Classifiers pets")
 st.write("Clasificador de mascotas. Introduzca los datos de su mascota y le diremos a que clase pertenece.")
-st.image("C:\Users\c.lopez.munoz\Documents\prueba_docker\escenario_6_streamlit_pets\img\foto-de-dibujo-colorido-gato-marrón.webp")
-
+st.image("img/gato.webp")
 # Cargar el modelo entrenado y las asignaciones para el color de ojos y el largo del pelo . 
 modelo = joblib.load("model/pets_model.joblib")
 with open("model/category_mapping.json") as f:
@@ -20,7 +20,7 @@ fur_length_values = category_mapping["fur_length"]
 # Crear los elementos para la entrada de datos.
 st.write("Introduzca los datos de su mascota")
 weight = st.number_input("Peso (Kg)", min_value=0.0, max_value=10.0, value=10.0)
-height = st.number_input("Altura (cm)", min_value=10.0 =, max_value=200.0, value=10.0)
+height = st.number_input("Altura (cm)", min_value=10.0, max_value=200.0, value=10.0)
 eye_color = st.selectbox("Color de ojos", ["Azul", "Marrón", "Gris", "Verde", ])
 fur_length = st.selectbox("Largo del pelo", ["Largo", "Medio", "Corto"])
 
@@ -46,5 +46,5 @@ input_df = pd.DataFrame([input_data], columns=columnas)
 if st.button("Predecir"):
     prediccion = modelo.predict(input_df)[0]
     prediction_map = {"dog": "Perro", "cat": "Gato", "rabbit": "Conejo"}
-    st.success(f'La mascota es un {prediction_map} ', icon="✅")
+    st.success(f'La mascota es un {prediction_map[prediccion]} ', icon="✅")
     st.balloons()
